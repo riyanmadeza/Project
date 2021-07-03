007e26738fc2c6a4473383c4516d0ab373452680
riyanmadeza <rmadeza@gmail.com>
2021-07-03 17:22:22 +0700

    Update LicenseController.php

--- Application/ucmas-api/app/Http/Controllers/LicenseController.php
+++ Application/ucmas-api/app/Http/Controllers/LicenseController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\License;
 use Illuminate\Http\Request;
 
 class LicenseController extends Controller
 {
-    //
+    public function search(Request $request)
+    {
+        $license = License::where('CABANG_CODE', $request->CABANG_CODE)->first();
+
+        return response()->json(['cabang' => $license], 200);
+    }
+
 }
